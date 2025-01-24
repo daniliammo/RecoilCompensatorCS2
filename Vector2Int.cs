4b399d474488380fd8ec661f4ad25dbd1a153efe
DANILIAMMO <67186086+daniliammo@users.noreply.github.com>
2025-01-25 01:39:39 +0500

    add operators to Vector2Int

--- Vector2Int.cs
+++ Vector2Int.cs
@@ -5,4 +5,14 @@ public class Vector2Int(int x, int y)
 {
     public int X = x;
     public int Y = y;
+    
+    public static Vector2Int operator /(Vector2Int vector, float d)
+    {
+        return new Vector2Int((int)(vector.X / d), (int)(vector.Y / d));
+    }
+    
+    public static Vector2Int operator +(Vector2Int vector, Vector2Int other)
+    {
+        return new Vector2Int(vector.X + other.X, vector.Y + other.Y);
+    }
 }
